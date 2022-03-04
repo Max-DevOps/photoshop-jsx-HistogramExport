1aa5315ab28edd9c034eec5f75368275d5d2ffe1
Tim Schofield <tim@maxcommunications.co.uk>
2022-03-04 11:46:17 +0000

    Some utils for min and max

--- HistogramExport.jsx
+++ HistogramExport.jsx
@@ -3,55 +3,138 @@
 
 try {
 
-	var filePath = "C:\\Users\\timot\\Tim\\Computing\\For Frank\\205 JSX Photoshop\\HistogramExport\\IWM_Histo_TestFixed.tif";
-	var fileInst1 = new File (filePath);
 
+	var inputFolder = Folder.selectDialog('Select the input folder', '');
 
-	if ( fileInst1 instanceof File ) {
-		alert("That IS a File object");
-	} else {
-		alert("That IS NOT a File object");
+	// With empty second arrgument it returns all file types
+	var myFilesAndFolders = scanSubFolders(inputFolder, "");
+
+	// returns an array of File objects
+	var myInputFiles = myFilesAndFolders[0];
+
+	var numFiles = myInputFiles.length;
+
+	alert("Files to test " + numFiles);
+
+	var docRef;
+	for(n = 0; n < numFiles; n++) {
+		docRef = open(myInputFiles[n]);
+		isFileDodgy(docRef);
 	}
 
-	var docRef = open(fileInst1);
 
-	// Returns an array of 256 numbers
-	var thisHistogram = docRef.channels[0].histogram;
-
-	for(n = 0; n < thisHistogram.length; n++) {
-		logInfo(thisHistogram[n]);
-	}
 
 
 } catch(error) {
 
-	alert(error);
+	alert("Error: " + error);
 
 } // eo catch
+/***********************************
 
-/**************************************
-***************************************/
-function logInfo(Txt) { 
+	Tests to see if the histogram for the file has notches in it.
+	If it has it is dodgy and isFileDodgy returns true, else false
 
-  // Writes the file to the desktop
-  var file = new File(Folder.desktop + "/ScriptLog.txt"); 
+ **********************************/
+ function isFileDodgy(docRef) {
 
-  file.open("e", "TEXT", "????"); 
+	// Returns an array of 256 numbers
+	var thisHistogram = docRef.channels[0].histogram;
 
-  file.seek(0,2); 
+	var smallestValueInList = timMin(thisHistogram);
+	var largestValueInList = timMax(thisHistogram);
+	alert(smallestValueInList + "  " + largestValueInList);
 
-  // After wring a line, gives it the correct lineFeed - what eytime a line is written?
-  if($.os.search(/windows/i)  != -1 ) {
-    file.lineFeed = 'windows';
-  } else {
-    file.lineFeed = 'macintosh'; 
-  }
+	var dynamicRange = largestValueInList - smallestValueInList;
 
-  file.writeln(Txt); 
+	// if two adacent brightnesses on the x axis are geater appart than this
+	// That counts as a notch 
+	var notchThesholdPercent = 15; 
 
-  file.close(); 
+	var notchThesholdAbsolute = (notchThesholdPercent/100) * dynamicRange;
 
-}; 
+	alert("notchThesholdAbsolute " + notchThesholdAbsolute);
+
+ }
+/***********************************
+ **********************************/
+ function timMin(listOfNumbers) {
+
+ 	var numNumbers = listOfNumbers.length;
+ 	var smallest = listOfNumbers[0];
+ 	var thisNum;
+
+ 	for(var n = 1; n < numNumbers; n++) {
+		thisNum = listOfNumbers[n];
+		if(thisNum < smallest) {
+			smallest = thisNum;
+		}
+ 	}
+
+ 	return(smallest);
+ }
+ /***********************************
+ **********************************/
+ function timMax(listOfNumbers) {
+
+ 	var numNumbers = listOfNumbers.length;
+ 	var largest = listOfNumbers[0];
+ 	var thisNum;
+
+ 	for(var n = 1; n < numNumbers; n++) {
+		thisNum = listOfNumbers[n];
+		if(thisNum > largest) {
+			largest = thisNum;
+		}
+ 	}
+
+ 	return(largest);
+}
+ /***********************************
+ **********************************/
+function scanSubFolders(tFolder, mask) 
+{ // folder object, RegExp or string
+
+   var sFolders = new Array();
+    var allFiles = new Array();
+    sFolders[0] = tFolder;
+
+    for (var j = 0; j < sFolders.length; j++)
+    { // loop through folders            
+       var procFiles = sFolders[j].getFiles(); // Note returns File instances
+
+        for (var i=0;i<procFiles.length;i++)
+        { // loop through this folder contents
+            if (procFiles[i] instanceof File )
+            {
+               // if(mask==undefined) {allFiles.push(procFiles[i].path);}// if no search mask collect all files /*  */
+                if (procFiles[i].fullName.search(mask) != -1) {allFiles.push(procFiles[i]);} // otherwise only those that match mask
+
+            }else if (procFiles[i] instanceof Folder){
+               sFolders.push(procFiles[i]);// store the subfolder
+                scanSubFolders(procFiles[i], mask);// search the subfolder/* */
+            } 
+        }
+    } 
+
+    // sFolders means subFolders
+   return [allFiles,sFolders]; 
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
