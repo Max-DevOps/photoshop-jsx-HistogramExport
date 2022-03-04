f2042d73446a299bd173341e257040682376cee6
Tim Schofield <tim@maxcommunications.co.uk>
2022-03-04 13:24:05 +0000

    wring log file

--- HistogramExport.jsx
+++ HistogramExport.jsx
@@ -6,6 +6,9 @@ try {
 
 	var inputFolder = Folder.selectDialog('Select the input folder', '');
 
+
+	alert(inputFolder + "/ScriptLog.txt");
+
 	// With empty second arrgument it returns all file types
 	var myFilesAndFolders = scanSubFolders(inputFolder, "");
 
@@ -17,12 +20,36 @@ try {
 	alert("Files to test " + numFiles);
 
 	var docRef;
+	var logFileName;
+	var logFilePath;
+	// We want to store the log file at the same level as input folder, not inside the input folder
+	// because this will crash the reading of files
+	var logFilePathExploded = inputFolder.fullName.split("/");
+	logFilePathExploded.pop();
+	logFilePath = logFilePathExploded.join("/");
+
+	alert(logFilePath + "/ScriptLog.txt");
+
 	for(n = 0; n < numFiles; n++) {
 		docRef = open(myInputFiles[n]);
-		isFileDodgy(docRef);
+		if(isFileDodgy(docRef) ) {
+			// alert(docRef.fullName + " IS dodgy");
+
+			// Log the bad files.
+			// LogFile written to the same folder as was selected for the input files
+			logInfo(logFileName, logFilePath, docRef.fullName);
+
+
+
+
+		} else {
+			// do nothing if the file is OK
+			//alert(docRef.name + " IS NOT dodgy");
+		}
+
+		activeDocument.close( SaveOptions.DONOTSAVECHANGES );
+
 	}
-
-
 
 
 } catch(error) {
@@ -40,10 +67,11 @@ try {
 
 	// Returns an array of 256 numbers
 	var thisHistogram = docRef.channels[0].histogram;
+	var numNumbers = thisHistogram.length;
 
 	var smallestValueInList = timMin(thisHistogram);
 	var largestValueInList = timMax(thisHistogram);
-	alert(smallestValueInList + "  " + largestValueInList);
+	// alert(smallestValueInList + "  " + largestValueInList);
 
 	var dynamicRange = largestValueInList - smallestValueInList;
 
@@ -51,9 +79,43 @@ try {
 	// That counts as a notch 
 	var notchThesholdPercent = 15; 
 
-	var notchThesholdAbsolute = (notchThesholdPercent/100) * dynamicRange;
+	// notchThesholdPercent, normalized for this particular image's dynamic range
+	var notchThesholdNormalized = (notchThesholdPercent/100) * dynamicRange;
 
-	alert("notchThesholdAbsolute " + notchThesholdAbsolute);
+	// alert("dynamicRange: " + dynamicRange + " notchThesholdNormalized: " + notchThesholdNormalized);
+
+	var firstSample;
+	var nextSample;
+	var thisDifference;
+	var notchCount = 0;
+	var numNotchesBeforeFileIsDodgy = 30;
+
+
+	// (numNumbers - 1) so that nextSample does not go outside the thisHistogram array
+	for(var n = 0; n < (numNumbers - 1); n++) {
+		firstSample = n;
+		nextSample = n + 1;
+
+		// could be negative or positive so use the absolute
+		thisDifference = thisHistogram[firstSample] - thisHistogram[nextSample]
+
+		thisDifference = Math.abs(thisDifference);
+		// alert("thisDifference: " + thisDifference);
+
+		if(thisDifference > notchThesholdNormalized) {
+			notchCount++;
+		}
+
+		if(notchCount > numNotchesBeforeFileIsDodgy) {
+			return(true); // it is dodgey
+		}
+
+
+	} // eo for
+
+	//alert("notchCount: " + notchCount);
+
+	return(false); // it is not dodgy
 
  }
 /***********************************
@@ -121,7 +183,30 @@ function scanSubFolders(tFolder, mask)
    return [allFiles,sFolders]; 
   
 }
+/**************************************
+ * This write one line of text, then closes the log file.
+***************************************/
+function logInfo(logFileName, logFilePath, Txt) { 
 
+  // Writes the file to the desktop
+  var file = new File(logFilePath + "/ScriptLog.txt"); 
+
+  file.open("e", "TEXT", "????"); 
+
+  file.seek(0,2); 
+
+  // After wring a line, gives it the correct lineFeed - what eytime a line is written?
+  if($.os.search(/windows/i)  != -1 ) {
+    file.lineFeed = 'windows';
+  } else {
+    file.lineFeed = 'macintosh'; 
+  }
+
+  file.writeln(Txt); 
+
+  file.close(); 
+
+}; 
 
 
 
